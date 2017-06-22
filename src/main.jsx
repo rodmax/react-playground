f2387a2612f805486f69d7a3cfeac89fa2370fd6
max.rodionov <max.rodionov@synesis.ru>
2017-06-23 00:01:37 +0300

    Enable i18n using react-intl library

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createAppStore} from 'store';
+import {IntlProvider} from 'react-intl';
+
 
 import App from 'app.jsx';
 
 const store = createAppStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <IntlProvider locale="en">
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </IntlProvider>,
     document.getElementById('root')
 );
--- src/topbar/topbar.jsx
+++ src/topbar/topbar.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 import {connect} from 'react-redux';
+import {defineMessages, injectIntl, intlShape} from 'react-intl';
 import {changeSearchText} from '../actions';
 import './topbar.scss';
 
 
+const msgs = defineMessages({
+    searchPlaceholder: {
+        id: 'Topbar.searchPlaceholder',
+        defaultMessage: 'search for locations...',
+    }
+});
+
+
 Topbar.propTypes = {
     onSearchTextChange: PropTypes.func.isRequired,
+    intl: intlShape.isRequired
 };
 
-function Topbar({onSearchTextChange}) {
+function Topbar({onSearchTextChange, intl}) {
     function onKeyPress(ev) {
         if (ev.key === 'Enter') {
             onSearchTextChange(ev.target.value);
         }
 
     }
+
     return <div className="c-topbar">
         <input
             type="text"
             name="name"
             onKeyPress={onKeyPress}
-            placeholder="search..."
+            placeholder={intl.formatMessage(msgs.searchPlaceholder)}
         />
     </div>;
 }
 
-export default connect(null, mapDispatchToProps)(Topbar);
+export default injectIntl(connect(null, mapDispatchToProps)(Topbar));
 
 function mapDispatchToProps(dispatch) {
     return {
