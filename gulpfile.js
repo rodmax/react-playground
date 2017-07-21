e84a8585bc94695d2df6f9bab3d7cf8bd0c124b9
max.rodionov <max.rodionov@synesis.ru>
2017-07-22 00:24:52 +0300

    Implement pre-push hook

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,24 @@ gulp.task('dev', shell.task('webpack-dev-server', {verbose: true}));
 gulp.task('webpack' , shell.task('webpack', {verbose: true}));
 
 gulp.task('eslint', shell.task('eslint --ext .js --ext .jsx ./', {verbose: true}));
+
+
+gulp.task('pre-push', ['eslint', 'webpack']);
+gulp.task('pre-commit', shell.task('echo "pre-commit hook not implemented yet :)"'));
+
+gulp.task('install-hooks', shell.task(
+    [].concat(
+        getShellCommandsToCreateHook('pre-commit'),
+        getShellCommandsToCreateHook('pre-push')
+    ),
+    {verbose: true}
+));
+
+
+function getShellCommandsToCreateHook(hook) {
+    return [
+        `echo '#!/bin/sh' > .git/hooks/${hook}`,
+        `echo gulp ${hook} >> .git/hooks/${hook}`,
+        `chmod a+x .git/hooks/${hook}`
+    ];
+}
