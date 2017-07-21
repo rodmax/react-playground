d2c0a53b7b4d308739940659ff9915d0ffa7db63
max.rodionov <max.rodionov@synesis.ru>
2017-07-22 02:34:31 +0300

    Return missing dev server port

--- devtools/env.js
+++ devtools/env.js
@@ -4,3 +4,5 @@ const env = module.exports = {};
 env.rootDir = path.join(__dirname, '..');
 env.buildDir = path.join(env.rootDir, 'docs'); // we use docs to able publish on gh-pages
 env.srcDir = path.join(env.rootDir, 'src');
+
+env.devServerPort = 3000;
