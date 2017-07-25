74ba7f32d16c2187ad1777824ad6fc1f69fb751c
max.rodionov <max.rodionov@synesis.ru>
2017-07-25 23:30:03 +0300

    Refactor folder structure / file names
    Conventions:
    - PascalCase for file and css selectors naming
    
    - BEM like conventions:
    .SomeBlock
    .SomeBlock__anyElement
    .SomeBlock.is-state
    .SomeBlock_anyElement.mod-someMode.has-anotherState
    
    - Modes/States not used without block/element in css file
    
    - Another helpers:
    .u-textTruncate  // utility
    .js-hookForJsCode  // used in javascript
    ._hackToPatchCss  // patch/workaround

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function Sidebar() {
     return (
         <div>
-            <h4 className="c-sidebar">I am sidebar</h4>
+            <h4 className="Sidebar">I am sidebar</h4>
         </div>
     );
 }
