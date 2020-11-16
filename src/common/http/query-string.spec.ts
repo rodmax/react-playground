89d1a2c7bd725aea6838945546945a955c4eb8c3
max.rodionov <rodmax@users.noreply.github.com>
2020-11-16 22:35:15 +0300

    feat(http): add query string serializer (#121)
    * feat(http): add query string serializer
    
    * refactor(http): use template literal instead of string concatenation

---
+++ src/common/http/query-string.spec.ts
@@ -0,0 +1,25 @@
+import { queryString } from './query-string'
+
+describe(queryString.name, () => {
+    it('serialize params with string/number value', () => {
+        expect(queryString({ num: 1, str: 'a' })).toStrictEqual('num=1&str=a')
+    })
+
+    it('serialize params with boolean value', () => {
+        expect(queryString({ flag: true })).toStrictEqual('flag=true')
+    })
+    it('serialize object as JSON', () => {
+        expect(queryString({ obj: { x: 1 } })).toStrictEqual(
+            `obj=${encodeURIComponent(JSON.stringify({ x: 1 }))}`
+        )
+    })
+    it('serialize array as multiple query string key-value pairs', () => {
+        expect(queryString({ a: [1, 'a', false] })).toStrictEqual('a=1&a=a&a=false')
+    })
+    it('skip "",null,undefined values', () => {
+        expect(queryString({ empty: '', null: null, undefined: undefined })).toStrictEqual('')
+    })
+    it('encode values', () => {
+        expect(queryString({ a: ['='], s: '=' })).toStrictEqual('a=%3D&s=%3D')
+    })
+})
